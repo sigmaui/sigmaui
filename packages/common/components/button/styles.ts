e70061bd3baa551a82f50b2b784b4df8672d5e5f
doong <donglh@gviet.vn>
2025-07-23 00:33:12 +0700

    update theming, sizes, variants

--- packages/common/components/button/styles.ts
+++ packages/common/components/button/styles.ts
@@ -1,21 +1,35 @@
 import { IProps, StylesProperties } from './types';
 
-export const styles = ({ theme = {} }: IProps<any>) => {
+export const styles = ({ theme = {}, size, variant }: IProps<any>) => {
   // console.log('theme', theme)
 
   return {
     wrapper: {
+      display: 'inline-flex',
+      alignItems: 'center',
       border: 0,
       paddingBlock: 8,
       paddingInline: 12,
       cursor: 'pointer',
-      backgroundColor: theme.colors?.base,
-      color: theme.colors?.text,
+      backgroundColor: !variant && theme.colors?.base,
+      color: !variant && theme.colors?.text,
       borderRadius: theme.base?.borderRadius,
+      size,
+      variant,
 
       ':hover': {
         backgroundColor: theme.fn?.lighten?.(theme.colors?.base, 0.1)
+      },
+
+      '&[disabled]': {
+        cursor: 'not-allowed'
       }
+    } as StylesProperties,
+    prefix: {
+      marginRight: 6
+    } as StylesProperties,
+    suffix: {
+      marginLeft: 6
     } as StylesProperties
   }
 }
